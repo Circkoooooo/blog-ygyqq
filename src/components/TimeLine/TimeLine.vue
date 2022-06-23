28df81b6d1bd3e3110279a81a57a07b7991dd58b
2389451262@qq.com <2389451262@qq.com>
2022-06-24 01:07:14 +0800

    chore: TimeLine组件

---
+++ src/components/TimeLine/TimeLine.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import './TimeLine.css'
+</script>
+
+<template>
+	<div class="outer_line">
+		<div class="line_support"></div>
+
+	</div>
+</template>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import './Home.css'
 import Overlay from '../../components/Overlay/Overlay.vue'
+import TimeLine from '../../components/TimeLine/TimeLine.vue'
 </script>
 <template>
 	<div class="background">
-		<div class="center">
-			<Overlay></Overlay>
+		<div class="timeline">
+			<TimeLine></TimeLine>
 		</div>
+		<!-- <div class="center">
+			<Overlay></Overlay>
+		</div> -->
 	</div>
 </template>
