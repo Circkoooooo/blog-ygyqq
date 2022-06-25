ca9d0bacca008ff43086895f046307f8483dc707
2389451262@qq.com <2389451262@qq.com>
2022-06-25 22:50:43 +0800

    style: update font.

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import Home from './pages/Home/Home.vue'
 import './styles/font.css'
 import './styles/global.css'
-createApp(Home).mount('#app')
+
+import router from './router'
+createApp(Home).use(router).mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import * as VueRouter from 'vue-router'
+import Home from '../pages/Home/Home.vue'
+import TimeLine from '../pages/TimeLine/TimeLine.vue'
+const routes = [
+	{ path: '/', component: Home },
+	{ path: '/timeline', component: TimeLine },
+]
+
+const router = VueRouter.createRouter({
+	history: VueRouter.createWebHashHistory(),
+	routes,
+})
+export default router
