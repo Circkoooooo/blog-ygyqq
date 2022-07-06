db394661c93b8f2f7b15e623009809828a5a12cc
2389451262@qq.com <2389451262@qq.com>
2022-07-06 22:07:21 +0800

    feat: new work.

--- src/pages/Work/Work.vue
+++ src/pages/Work/Work.vue
@@ -16,7 +16,15 @@ const contentList: ContentType[] = [{
 	time: '2022-05-25',
 	detail: '提供一个用于绘制节点直接的贝塞尔曲线的api',
 	link: 'https://github.com/Circkoooooo/vmind'
-}]
+},
+{
+	title: 'Mirai-circlebot',
+	imgPath: 'img/circlebot.jpg',
+	time: '2022-07-06',
+	detail: '自动生成所有配置文件的基于mirai-ts的mirai机器人',
+	link: 'https://github.com/Circkoooooo/Mirai-Generator'
+}
+]
 
 const list = computed(() => {
 	const newList: ContentType[] = []
